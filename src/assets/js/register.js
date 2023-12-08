c3473b9f2926c80dbfb70e5e8980950e5a0214d1
daffa09 <daffa.fathan9@gmail.com>
2023-12-08 20:58:27 +0700

    mini improve register

--- src/assets/js/register.js
+++ src/assets/js/register.js
@@ -6,7 +6,7 @@ function registerUser() {
 
       // Validasi formulir
       if (!username || !email || !password) {
-        showToast('Gagal mendaftar. Harap isi semua kolom.', 'text-bg-danger');
+        showToast('Create Failed. Please fill all coloumn', 'text-bg-danger');
         return;
       }
 
@@ -27,24 +27,42 @@ function registerUser() {
       // Simpan data ke dalam localStorage
       localStorage.setItem('users', JSON.stringify(existingUsers));
 
-      showToast('Pendaftaran berhasil. Data tersimpan.', 'text-bg-success');
+      showToast('Create Successful. Data saved.', 'text-bg-success');
+      setTimeout(() => {
+        window.location.assign('/index.html');
+      }, 1000);
     }
 
-    function showToast(message, toastColor) {
-      const toastContainer = document.getElementById('toastContainer');
-      toastContainer.innerHTML = `
-      <div class="toast align-items-center ${toastColor} rounded-2 p-2 w-auto" role="alert" aria-live="assertive"
-            aria-atomic="true">
-            <div class="d-flex">
-              <div class="toast-body">
-                ${message}
-              </div>
-              <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
-                aria-label="Close"></button>
-            </div>
-          </div>
-    `;
+function showToast(message, toastColor) {
+  const toastContainer = document.getElementById('toastContainer');
+  toastContainer.innerHTML = `
+    <div class="toast align-items-center ${toastColor} rounded-2 p-2 w-auto" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+        <div class="toast-body">
+          ${message}
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+    </div>
+  `;
 
-      const toast = new bootstrap.Toast(document.querySelector('.toast'));
-      toast.show();
-    }
+  const toast = new bootstrap.Toast(document.querySelector('.toast'));
+  toast.show();
+}
+
+function googleLogin() {
+  const toastContainer = document.getElementById('toastContainer');
+  toastContainer.innerHTML = `
+    <div class="toast align-items-center text-bg-danger rounded-2 p-2 w-auto" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+        <div class="toast-body">
+          Feature is not available yet.
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+    </div>
+  `;
+
+  const toast = new bootstrap.Toast(document.querySelector('.toast'));
+  toast.show();
+}
